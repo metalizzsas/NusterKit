55139fd99b7933e81698dc617767ebc0301d37cc
Kworz <romain.sapet@metalizz.com>
2022-01-20 19:42:44 +0100

    Added an error page to fallback to /app

---
+++ src/routes/__error.svelte
@@ -0,0 +1,27 @@
+<script context="module" lang="ts">
+	import '$lib/app.css';
+	import type { ErrorLoad } from '@sveltejs/kit';
+	export const load: ErrorLoad = (ctx) => {
+		return {
+			props: {
+				title: `${ctx.status}: ${ctx.error?.message}`,
+			},
+		};
+	};
+</script>
+
+<script lang="ts">
+	export let title: string;
+</script>
+
+<main class="text-gray-800">
+	<h1 class="text-xl">Failed to render this page</h1>
+	<h2 class="text-sm">{title}</h2>
+
+	<a
+		href="/app"
+		class="inline-block py-2 px-5 rounded-xl font-semibold bg-orange-500 text-white mt-3"
+	>
+		Return to main App
+	</a>
+</main>

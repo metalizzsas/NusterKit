9ae9d1c19f6161a8bd2d2d658c287896606aef24
Kworz <romain.sapet@metalizz.com>
2022-01-27 12:09:00 +0100

    Modals: Removed backblur due to performance issues

--- src/lib/components/modals/modalcontent.svelte
+++ src/lib/components/modals/modalcontent.svelte
@@ -17,7 +17,7 @@
 
 {#if shown}
 	<div
-		class="absolute top-0 right-0 left-0 bottom-0 backdrop-blur-sm backdrop-brightness-50 z-50"
+		class="absolute top-0 right-0 left-0 bottom-0 backdrop-grayscale-50 z-50"
 		in:fade={{ duration: 100 }}
 		out:fade={{ duration: 100 }}
 	>

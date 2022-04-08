e0352d1a2ba9e40f01af6b65affc2932c8ee750f
Kworz <romain.sapet@metalizz.com>
2022-04-08 11:15:15 +0200

    Balena: Updated compose file

---
+++ clone.sh
@@ -0,0 +1,3 @@
+git clone git@github.com:metalizzsas/NusterTurbine.git nuster-turbine
+git clone git@github.com:metalizzsas/NusterTurbineMachines.git nuster-turbine-machines
+git clone git@github.com:metalizzsas/NusterDesktop.git nuster-desktop

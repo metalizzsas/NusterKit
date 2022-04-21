cc41ba2bc53017255138263ad34379d648eb146f
Kworz <romain.sapet@metalizz.com>
2022-04-21 18:49:38 +0200

    Updated version

--- clone.sh
+++ clone.sh
@@ -1,3 +1,4 @@
+rm -rf nuster-*
 git clone git@github.com:metalizzsas/NusterTurbine.git nuster-turbine
-git clone git@github.com:metalizzsas/NusterTurbineMachines.git nuster-turbine-machines
+git clone git@github.com:metalizzsas/NusterTurbineMachines.git nuster-turbine/nuster-turbine-machines
 git clone git@github.com:metalizzsas/NusterDesktop.git nuster-desktop

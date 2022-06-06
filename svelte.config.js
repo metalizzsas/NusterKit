e2df86a25dba14399c2ccd163ab4627d182948aa
Kworz <romain.sapet@metalizz.com>
2022-06-07 01:01:55 +0200

    PKG: Updated building schema

--- svelte.config.js
+++ svelte.config.js
@@ -3,13 +3,15 @@ import adapter from "@sveltejs/adapter-static";
 import { VitePWA } from "vite-plugin-pwa";
 import { pwaConfiguration } from "./pwa-configuration.js";
 
+console.log(`Vite bundled: ${process.env.VITE_BUNDLED}`);
+
 const config = {
   kit: {
     adapter: adapter({
       fallback: "index.html"
     }),
     vite: {
-      plugins: VitePWA(pwaConfiguration)
+      plugins: [VitePWA(pwaConfiguration)]
     }
   },
   preprocess: sveltePreprocess()

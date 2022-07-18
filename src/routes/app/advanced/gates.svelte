ae9870e70f27fb86463ebdc37946f312aa332f7e
Kworz <romain.sapet@metalizz.com>
2022-07-18 16:15:09 +0200

    IO: Gates are now tabbed

--- src/routes/app/advanced/gates.svelte
+++ src/routes/app/advanced/gates.svelte
@@ -7,7 +7,6 @@
 	import { navActions, navBackFunction, navTitle, useNavContainer } from '$lib/utils/navstack';
 	import Navcontainer from '$lib/components/navigation/navcontainer.svelte';
 	import Navcontainertitle from '$lib/components/navigation/navcontainertitle.svelte';
-	import Navcontainersubtitle from '$lib/components/navigation/navcontainersubtitle.svelte';
 
 	$: gates = $machineData.io;
 
@@ -19,74 +18,100 @@
 	$navBackFunction = () => goto('/app/advanced');
 	$navActions = [];
 	$useNavContainer = false;
+
+	let tab = 'in';
 </script>
 
-{#each ['in', 'out'] as bus}
+<Navcontainer>
+	<div class="grid grid-cols-2 gap-4">
+		<div
+			on:click={() => (tab = 'in')}
+			class=" bg-gradient-to-br {tab == 'in'
+				? 'from-indigo-500 to-indigo-600'
+				: 'from-gray-500 to-gray-600'}  py-3 px-5 text-white font-semibold rounded-xl text-center transition-all hover:opacity-90 duration-200 ease-in-out cursor-pointer"
+		>
+			<div class="flex flex-row self-center gap-2 justify-center w-full">
+				<svg
+					id="glyphicons-basic"
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 32 32"
+					class="h-5 w-5 fill-white self-center"
+				>
+					<path
+						id="square-download"
+						d="M27,8.5v15A3.50424,3.50424,0,0,1,23.5,27H8.5A3.50424,3.50424,0,0,1,5,23.5V8.5A3.50424,3.50424,0,0,1,8.5,5h4.13293l-.407,3H8.5a.50641.50641,0,0,0-.5.5v15a.50641.50641,0,0,0,.5.5h15a.50641.50641,0,0,0,.5-.5V8.5a.50641.50641,0,0,0-.5-.5H19.77411l-.407-3H23.5A3.50424,3.50424,0,0,1,27,8.5ZM11.18994,13.82385l3.98859,5.74628a.99994.99994,0,0,0,1.64294,0l3.98859-5.74628a1,1,0,0,0-.82147-1.57019H18.2467l-1.20849-9.5415a.5.5,0,0,0-.49829-.4585H15.46008a.5.5,0,0,0-.49829.4585l-1.20849,9.5415H12.01141A1,1,0,0,0,11.18994,13.82385Z"
+					/>
+				</svg>
+
+				{$_('gates.bus.in')}
+			</div>
+		</div>
+		<div
+			on:click={() => (tab = 'out')}
+			class=" bg-gradient-to-br {tab == 'out'
+				? 'from-indigo-500 to-indigo-600'
+				: 'from-gray-500 to-gray-600'} py-3 px-5 text-white font-semibold rounded-xl text-center transition-all hover:opacity-90 duration-200 ease-in-out cursor-pointer"
+		>
+			<div class="flex flex-row self-center gap-2 justify-center w-full">
+				<svg
+					id="glyphicons-basic"
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 32 32"
+					class="h-5 w-5 fill-white self-center"
+				>
+					<path
+						id="square-upload"
+						d="M27,8.5v15A3.50424,3.50424,0,0,1,23.5,27H8.5A3.50424,3.50424,0,0,1,5,23.5V8.5A3.50424,3.50424,0,0,1,8.5,5h2.63623L9.54785,7.28809A3.009,3.009,0,0,0,9.18549,8H8.5a.50641.50641,0,0,0-.5.5v15a.50641.50641,0,0,0,.5.5h15a.50641.50641,0,0,0,.5-.5V8.5a.50641.50641,0,0,0-.5-.5h-.6853a3.00838,3.00838,0,0,0-.36158-.71094L20.86407,5H23.5A3.50424,3.50424,0,0,1,27,8.5ZM12.01141,10H13.7533l1.20849,9.5415A.5.5,0,0,0,15.46008,20H16.54a.5.5,0,0,0,.49823-.4585L18.2467,10h1.74189a1,1,0,0,0,.82147-1.57019L16.82153,2.68353a1,1,0,0,0-1.643,0L11.18994,8.42981A1,1,0,0,0,12.01141,10Z"
+					/>
+				</svg>
+
+				{$_('gates.bus.out')}
+			</div>
+		</div>
+	</div>
+</Navcontainer>
+
+{#each [...new Set(gates.filter((g) => g.bus == tab).map((g) => g.category))] as cat}
 	<Navcontainer>
-		<Navcontainertitle>{$_('gates.bus.' + bus)}</Navcontainertitle>
-		<div class="flex flex-col">
-			{#each gates
-				.filter((g) => g.bus == bus)
-				.map((io) => {
-					let z = io.name.split('-');
-					return z[0] == io.name ? 'generic' : gates.filter((h) => h.bus == bus && h.name.startsWith(z[0])).length == 1 ? 'generic' : z[0];
-				})
-				.sort((a, b) => a.localeCompare(b))
-				.filter((i, p, a) => a.indexOf(i) == p)
-				.sort((a, b) => (a == 'generic' ? -1 : 1)) as cat}
-				{#if gates
-					.filter((g) => g.bus == bus)
-					.map((io) => {
-						let z = io.name.split('-');
-						return z[0] == io.name ? 'generic' : gates.filter((h) => h.bus == bus && h.name.startsWith(z[0])).length == 1 ? 'generic' : z[0];
-					})
-					.sort((a, b) => a.localeCompare(b))
-					.filter((i, p, a) => a.indexOf(i) == p)
-					.sort((a, b) => (a == 'generic' ? -1 : 1)).length > 1}
-					<Navcontainersubtitle>{$_('gates.categories.' + cat)}</Navcontainersubtitle>
-				{/if}
+		<Navcontainertitle>{$_('gates.categories.' + cat)}</Navcontainertitle>
 
-				<div class="flex flex-col gap-2 mb-6 last:mb-0">
-					{#each gates.filter((g, i, a) => g.bus == bus && (g.name.startsWith(cat) || (cat == 'generic' && gates.filter((h) => h.bus == bus && h.name.startsWith(g.name.split('-')[0])).length == 1) || (cat == 'generic' && g.name.split('-').length == 1))) as output, index}
-						<div
-							class="text-white flex flex-row justify-between gap-4 bg-zinc-500 py-2 pl-3 pr-2 rounded-xl font-semibold"
-						>
-							<span class="w-1/3 text-ellipsis">
-								{$_('gates.names.' + output.name)}
-							</span>
-							{#if output.size == 'bit'}
-								<Toggle
+		<div class="flex flex-col gap-2 mb-6 last:mb-0">
+			{#each gates.filter((g) => g.bus == tab && g.category == cat) as output, index}
+				<div
+					class="text-white flex flex-row justify-between gap-4 bg-zinc-500 py-2 pl-3 pr-2 rounded-xl font-semibold"
+				>
+					<span class="w-1/3 text-ellipsis">
+						{$_('gates.names.' + output.name)}
+					</span>
+					{#if output.size == 'bit'}
+						<Toggle
+							bind:value={output.value}
+							on:change={(val) => update(output.name, val.detail.value)}
+							locked={tab == 'in'}
+						/>
+					{:else}
+						<div class="flex flex-col md:flex-row gap-4 align-center">
+							{#if tab == 'out'}
+								<input
+									type="range"
+									min="0"
+									max="100"
+									on:input={() => {
+										$lockMachineData = true;
+									}}
 									bind:value={output.value}
-									on:change={(val) => update(output.name, val.detail.value)}
-									locked={bus == 'in'}
+									on:change={() => {
+										$lockMachineData = false;
+										update(output.name, output.value);
+									}}
 								/>
-							{:else}
-								<div class="flex flex-col md:flex-row gap-4 align-center">
-									{#if bus == 'out'}
-										<input
-											type="range"
-											min="0"
-											max="100"
-											on:input={() => {
-												$lockMachineData = true;
-											}}
-											bind:value={output.value}
-											on:change={() => {
-												$lockMachineData = false;
-												update(output.name, output.value);
-											}}
-										/>
-									{/if}
-
-									<span
-										class="py-1 px-2 rounded-full bg-white text-gray-800 text-sm"
-									>
-										{Math.ceil(output.value)} %
-									</span>
-								</div>
 							{/if}
+
+							<span class="py-1 px-2 rounded-full bg-white text-gray-800 text-sm">
+								{Math.ceil(output.value)} %
+							</span>
 						</div>
-					{/each}
+					{/if}
 				</div>
 			{/each}
 		</div>

407a0fc25122cc5d004a9121d47c3bf8612c1290
Kworz <romain.sapet@metalizz.com>
2022-08-18 18:16:36 +0200

    Passive block rework

--- src/controllers/passives/Passive.ts
+++ src/controllers/passives/Passive.ts
@@ -2,8 +2,10 @@ import { model, Schema } from "mongoose";
 import { IPassive } from "../../interfaces/IPassive";
 import { Machine } from "../../Machine";
 
-export class Passive implements IPassive
-{
+const MAXPASSIVELOGPOINTSSTORED = 100;
+const MAXPASSIVELOGPOINTSSHOWN = 25;
+
+export class Passive implements IPassive {
     name: string;
 
     state: boolean;
@@ -24,8 +26,9 @@ export class Passive implements IPassive
     machine: Machine;
     controlTimer?: NodeJS.Timer;
 
-    constructor(machine: Machine, obj: IPassive)
-    {
+    logPassivePoints: IPassiveStoredLogData[] = new Array<IPassiveStoredLogData>(MAXPASSIVELOGPOINTSSTORED);
+
+    constructor(machine: Machine, obj: IPassive) {
         //basic informations
         this.name = obj.name;
         this.target = obj.target;
@@ -41,17 +44,43 @@ export class Passive implements IPassive
         this.state = false;
 
         this.createOrModifyPassiveDocument();
+
     }
 
-    async createOrModifyPassiveDocument()
-    {
+    async addLogDataPoint() {
         const doc = await PassiveModel.findOne({ name: this.name });
 
-        if(doc === null)
+        if(doc !== null)
         {
+
+            const newlogPoint = {
+                targetValue: this.target,
+                interpolatedSensorsValue: this.currentValue,
+
+                state: this.state,
+                time: new Date()
+            } as IPassiveStoredLogData;
+
+            //add new logpoint
+            this.logPassivePoints.push(newlogPoint);
+            //remove first index
+            if(this.logPassivePoints.length >= MAXPASSIVELOGPOINTSSTORED)
+                this.logPassivePoints.splice(0, 1);
+
+            doc.logData = this.logPassivePoints;
+            await doc.save();
+        }
+
+        setTimeout(this.addLogDataPoint.bind(this), this.state ? 60000 : 15000);
+    }
+
+    async createOrModifyPassiveDocument() {
+        const doc = await PassiveModel.findOne({ name: this.name }).slice("logData", -MAXPASSIVELOGPOINTSSTORED);
+
+        if (doc === null) {
             PassiveModel.create({
                 name: this.name,
-                target: this.target, 
+                target: this.target,
                 state: this.state
             });
         }
@@ -59,19 +88,72 @@ export class Passive implements IPassive
         {
             this.target = doc.target;
             this.state = doc.state;
+
+            this.toggle(this.state);
+
+            this.logPassivePoints = doc.logData;
         }
+
+        this.addLogDataPoint();
     }
 
-    async saveDocument()
-    {
+    async saveDocument() {
         const doc = await PassiveModel.findOne({ name: this.name });
 
-        if(doc !== null)
-        {
+        if (doc !== null) {
             doc.target = this.target;
             doc.state = this.state;
 
             await doc.save();
+        }
+    }
+
+    private regulationLoop() {
+        if (this.currentValue > this.target) {
+            //disables positive actuators
+            if (typeof this.actuators.plus === "string") {
+                this.machine.ioController.gFinder(this.actuators.plus)?.write(this.machine.ioController, 0);
+            }
+            else {
+                for (const actuator of this.actuators.plus) {
+                    this.machine.ioController.gFinder(actuator)?.write(this.machine.ioController, 0);
+                }
+            }
+
+            //if this passive has minus Actuators enable them
+            if (this.actuators.minus !== undefined) {
+                if (typeof this.actuators.minus === "string") {
+                    this.machine.ioController.gFinder(this.actuators.minus)?.write(this.machine.ioController, 1);
+                }
+                else {
+                    for (const actuator of this.actuators.minus) {
+                        this.machine.ioController.gFinder(actuator)?.write(this.machine.ioController, 1);
+                    }
+                }
+            }
+        }
+        else {
+            //if this passive has minus Actuators disable them
+            if (this.actuators.minus !== undefined) {
+                if (typeof this.actuators.minus === "string") {
+                    this.machine.ioController.gFinder(this.actuators.minus)?.write(this.machine.ioController, 0);
+                }
+                else {
+                    for (const actuator of this.actuators.minus) {
+                        this.machine.ioController.gFinder(actuator)?.write(this.machine.ioController, 0);
+                    }
+                }
+            }
+
+            //enable positive actuators
+            if (typeof this.actuators.plus === "string") {
+                this.machine.ioController.gFinder(this.actuators.plus)?.write(this.machine.ioController, 1);
+            }
+            else {
+                for (const actuator of this.actuators.plus) {
+                    this.machine.ioController.gFinder(actuator)?.write(this.machine.ioController, 1);
+                }
+            }
         }
     }
 
@@ -80,128 +162,56 @@ export class Passive implements IPassive
      * @param value true enables false disables
      * @param manual Enable manual modes 
      */
-    toggle(value: boolean, manual = true)
-    {
+    toggle(value: boolean, manual = true) {
         this.state = value;
         this.saveDocument();
 
-        if(this.state === true)
-        {
-            if(this.controlTimer !== undefined)
-            {
+        if (this.state === true) {
+            this.machine.logger.info("Passive: Enabling " + this.name);
+
+            if (this.controlTimer !== undefined) {
                 clearInterval(this.controlTimer);
             }
-            else
-            {
-                //if manual modes are defined, toggle them to 1 & lock them.
-                if(this.manualModes && manual)
-                {
-                    if(typeof this.manualModes == "string")
-                    {
-                        const manual  = this.machine.manualmodeController.find(this.manualModes);
+
+            //if manual modes are defined, toggle them to 1 & lock them.
+            if (this.manualModes && manual) {
+                if (typeof this.manualModes == "string") {
+                    const manual = this.machine.manualmodeController.find(this.manualModes);
+
+                    console.log(manual);
+
+                    manual?.toggle(1);
+                    manual?.lock();
+                }
+                else {
+                    for (const mn of this.manualModes) {
+                        const manual = this.machine.manualmodeController.find(mn);
 
                         manual?.toggle(1);
                         manual?.lock();
                     }
-                    else
-                    {
-                        for(const mn of this.manualModes)
-                        {
-                            const manual = this.machine.manualmodeController.find(mn);
-
-                            manual?.toggle(1);
-                            manual?.lock();
-                        }
-                    }
                 }
-
-                this.controlTimer = setInterval(() => {
-                    if(this.target > this.currentValue)
-                    {
-                        //disables positive actuators
-                        if(typeof this.actuators.plus === "string")
-                        {
-                            this.machine.ioController.gFinder(this.actuators.plus)?.write(this.machine.ioController, 0);
-                        }
-                        else
-                        {
-                            for(const actuator of this.actuators.plus)
-                            {
-                                this.machine.ioController.gFinder(actuator)?.write(this.machine.ioController, 0);
-                            }
-                        }
-
-                        //if this passive has minus Actuators enable them
-                        if(this.actuators.minus !== undefined)
-                        {
-                            if(typeof this.actuators.minus === "string")
-                            {
-                                this.machine.ioController.gFinder(this.actuators.minus)?.write(this.machine.ioController, 1);
-                            }
-                            else
-                            {
-                                for(const actuator of this.actuators.minus)
-                                {
-                                    this.machine.ioController.gFinder(actuator)?.write(this.machine.ioController, 1);
-                                }
-                            } 
-                        }
-                    }
-                    else
-                    {
-                        //if this passive has minus Actuators disable them
-                        if(this.actuators.minus !== undefined)
-                        {
-                            if(typeof this.actuators.minus === "string")
-                            {
-                                this.machine.ioController.gFinder(this.actuators.minus)?.write(this.machine.ioController, 0);
-                            }
-                            else
-                            {
-                                for(const actuator of this.actuators.minus)
-                                {
-                                    this.machine.ioController.gFinder(actuator)?.write(this.machine.ioController, 0);
-                                }
-                            } 
-                        }
-
-                        //enable positive actuators
-                        if(typeof this.actuators.plus === "string")
-                        {
-                            this.machine.ioController.gFinder(this.actuators.plus)?.write(this.machine.ioController, 1);
-                        }
-                        else
-                        {
-                            for(const actuator of this.actuators.plus)
-                            {
-                                this.machine.ioController.gFinder(actuator)?.write(this.machine.ioController, 1);
-                            }
-                        }
-                    }
-                }, 5000);
             }
+
+            this.controlTimer = setInterval(this.regulationLoop.bind(this), 5000);
         }
-        else
-        {
-            if(this.controlTimer !== undefined)
-            {
+        else {
+            this.machine.logger.info("Passive: Disabling " + this.name);
+
+            if (this.controlTimer !== undefined) {
                 clearInterval(this.controlTimer);
             }
 
             //if manual modes are defined, toggle them to 0 & unlock them.
-            if(this.manualModes && manual)
-            {
-                if(typeof this.manualModes == "string")
-                {
-                    const manual  = this.machine.ioController.machine.manualmodeController.find(this.manualModes);
+            if (this.manualModes && manual) {
+                if (typeof this.manualModes == "string") {
+                    const manual = this.machine.ioController.machine.manualmodeController.find(this.manualModes);
 
                     manual?.toggle(0);
                     manual?.unlock();
                 }
-                else
-                {
-                    for(const mn of this.manualModes)
-                    {
+                else {
+                    for (const mn of this.manualModes) {
                         const manual = this.machine.ioController.machine.manualmodeController.find(mn);
 
                         manual?.toggle(0);
@@ -212,48 +222,62 @@ export class Passive implements IPassive
         }
     }
 
-    setTarget(value: number)
-    {
+    setTarget(value: number) {
         this.target = value;
         this.saveDocument();
     }
 
-    get currentValue(): number
-    {
+    get currentValue(): number {
         //if there is multiple sensors values return a average values of all of them else return only the sensors value
-        if(typeof this.sensors === "string")
-        {
+        if (typeof this.sensors === "string") {
             return this.machine.ioController.gFinder(this.sensors)?.value ?? 0;
         }
-        else
-        {
+        else {
             const values: number[] = this.sensors.map(s => this.machine.ioController.gFinder(s)?.value ?? 0);
             return values.reduce((count, x) => count + x) / this.sensors.length;
         }
     }
 
-    toJSON()
-    {
+    toJSON() {
         return {
             name: this.name,
             target: this.target,
             current: this.currentValue,
-            state: this.state
+            state: this.state,
+            logData: this.logPassivePoints.slice(0, (MAXPASSIVELOGPOINTSSTORED - MAXPASSIVELOGPOINTSSHOWN))
         }
     }
 }
 
-interface IPassiveStored
-{
+interface IPassiveStored {
     name: string;
     target: number;
     state: boolean;
+
+    logData: IPassiveStoredLogData[]
 }
 
+interface IPassiveStoredLogData {
+    time?: Date,
+    state?: boolean
+
+    targetValue: number,
+    interpolatedSensorsValue: number,
+}
+
+const PassiveLogDataSchema = new Schema<IPassiveStoredLogData>({
+    time: { type: Date, default: Date.now },
+    targetValue: { type: Number, required: true },
+    interpolatedSensorsValue: { type: Number, required: true },
+    state: { type: Boolean, default: false}
+});
+
 const PassiveSchema = new Schema<IPassiveStored>({
-    name: {type: String, required: true},
-    target: {type: Number, required: true},
-    state: {type: Boolean, required: true}
+    name: { type: String, required: true },
+    target: { type: Number, required: true },
+    state: { type: Boolean, required: true },
+
+    logData: { type: [PassiveLogDataSchema], required: false, default: []}
 });
 
 const PassiveModel = model("passive", PassiveSchema);
--- src/pbr/ProgramBlocks/PassiveProgramBlock.ts
+++ src/pbr/ProgramBlocks/PassiveProgramBlock.ts
@@ -28,6 +28,8 @@ export class PassiveProgramBlock extends ProgramBlock implements IPassiveProgram
         {
             passive.toggle(this.params[1].data() == 1, false);
             passive.setTarget(this.params[2].data());
+
+            this.pbrInstance.machine.logger.info("Passive: Setting " + this.params[0].data() + " state to " + this.params[1].data() + " and target to " + this.params[2].data());
         }
     }
 }

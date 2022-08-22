270a2ba4bfa4f5ed66eb3e1b247c306eb9b85195
Kworz <romain.sapet@metalizz.com>
2022-08-22 09:16:43 +0200

    Enabled pre-rendering

--- svelte.config.js
+++ svelte.config.js
@@ -8,10 +8,7 @@ const config = {
   kit: {
     adapter: adapter({
       fallback: "index.html",
-    }),
-    prerender: {
-      enabled: false
-    }
+    })
   },
   preprocess: sveltePreprocess()
 };

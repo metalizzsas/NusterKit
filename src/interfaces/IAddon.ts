9e21be10c566c7c380e291902dce126551bd332b
Kworz <romain.sapet@metalizz.com>
2022-09-21 16:40:21 +0200

    [Addon] Adjusted Schema

--- src/interfaces/IAddon.ts
+++ src/interfaces/IAddon.ts
@@ -1,21 +1,3 @@
-import { IMachine } from "./IMachine";
-
-type Prev = [never, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-    11, 12, 13, 14, 15, 16, 17, 18, 19, 20, ...0[]]
-
-type Join<K, P> = K extends string | number ?
-    P extends string | number ?
-    `${K}${"" extends P ? "" : "."}${P}`
-    : never : never;
-
-type Paths<T, D extends number = 10> = [D] extends [never] ? never : T extends object ?
-    { [K in keyof T]-?: K extends string | number ?
-        `${K}` | Join<K, Paths<T[K], Prev[D]>>
-        : never
-    }[keyof T] : ""
-
-export type IMachinePaths = Paths<IMachine>
-
 export declare interface IAddon
 {
     /** Addon name, should be the same as the Json file holding him */
@@ -26,8 +8,20 @@ export declare interface IAddon
 
 interface IAddonContent
 {
-    /** Path were the content is added */
-    path: IMachinePaths;
-    /** Content replaced or inserted to this category */
+    /** 
+     * Path were the content is added
+     * @warning As Template literals are **not supported** by json schema be careful about the path
+     */
+    path: string;
+
+    /**
+     * Insertion mode
+     * @defaultValue `merge`
+     */
+    mode: "replace" | "merge";
+    /** 
+     * Content replaced or inserted to this category
+     * @warning Content is not type aware about what you are adding here, **⚠ be careful**
+     */
     content: unknown;
 }

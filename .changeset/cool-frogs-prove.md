1438614a623096524cc3c1a8edf954e98da53e82
Kworz <romain.sapet@metalizz.com>
2022-11-01 19:38:11 +0100

    add: /remote route

---
+++ .changeset/cool-frogs-prove.md
@@ -0,0 +1,5 @@
+---
+"@metalizzsas/nuster-desktop": patch
+---
+
+added /remote route to access nuster desktop via remote ip
---
+++ packages/desktop/src/routes/remote/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { goto } from "$app/navigation";
+	import { onMount } from "svelte";
+
+    export const ssr = false;
+
+    onMount(() => {
+        const remoteIpAccess = window.location.hostname;
+        window.localStorage.setItem("ip", remoteIpAccess);
+
+        void goto("/app");
+    });
+
+</script>

be680fae105ce85b20e1bc44a712cabd48bf9c89
Kworz <romain.sapet@metalizz.com>
2022-11-30 00:54:56 +0100

    fix: lint & packages updates

---
+++ simulation/simulation-server/README.md
@@ -0,0 +1,11 @@
+# Simulation Server
+
+Simulation Tool server. Should be only used in dev mode
+
+## Running
+
+```bash
+npm run dev
+```
+
+Runs the simulation server on port `4081`
--- simulation/simulation-ui/src/routes/+page.ts
+++ simulation/simulation-ui/src/routes/+page.ts
@@ -1,12 +1,11 @@
 import type { PageLoad } from './$types';
-import type { IOGates } from "@metalizzsas/nuster-typings/build/spec/iogates";
+import type { IOGates } from '@metalizzsas/nuster-typings/build/spec/iogates';
 
 export const load: PageLoad = async ({ fetch }) => {
+	const req = await fetch('http://127.0.0.1:4081/io');
+	const gates = (await req.json()) as IOGates[];
 
-    const req = await fetch("http://127.0.0.1:4081/io");
-    const gates = (await req.json()) as IOGates[];
-
-    return {
-        gates
-    };
+	return {
+		gates
+	};
 };

ae4fc39b0050dc5248cc02c505d7e7d28bbee321
Kworz <romain.sapet@metalizz.com>
2022-11-30 00:39:30 +0100

    new: added simulation tools

---
+++ simulation/simulation-ui/src/routes/+page.ts
@@ -0,0 +1,12 @@
+import type { PageLoad } from './$types';
+import type { IOGates } from "@metalizzsas/nuster-typings/build/spec/iogates";
+
+export const load: PageLoad = async ({ fetch }) => {
+
+    const req = await fetch("http://127.0.0.1:4081/io");
+    const gates = (await req.json()) as IOGates[];
+
+    return {
+        gates
+    };
+};

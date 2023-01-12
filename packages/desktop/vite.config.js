59bca0d0af3bbc9fc13f3d6e149959ab14a3a615
Kworz <romain.sapet@metalizz.com>
2023-01-12 11:27:44 +0100

    fix: updated port to be 4081 for desktop app

--- packages/desktop/vite.config.js
+++ packages/desktop/vite.config.js
@@ -2,8 +2,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type import('vite').UserConfig */
 export default {
-        plugins: [sveltekit()],
-        ssr: {
-                noExternal: ["@fontsource/inter"],
-        }
+    plugins: [sveltekit()],
+    ssr: {
+        noExternal: ["@fontsource/inter"],
+    },
+    server: {
+        port: 4081,
+        host: "0.0.0.0",
+    },
 };

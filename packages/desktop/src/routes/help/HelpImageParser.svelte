6d9414be869f4d8f6bc26778467d9b82796119f4
Kworz <romain.sapet@metalizz.com>
2023-01-16 16:46:30 +0100

    feat: Markdown images are now zoomable

--- packages/desktop/src/routes/help/HelpImageParser.svelte
+++ packages/desktop/src/routes/help/HelpImageParser.svelte
@@ -1,14 +1,27 @@
 <script lang="ts">
+	import Flex from "$lib/components/layout/flex.svelte";
 	import { lang } from "$lib/utils/stores/settings";
+	import { ArrowsPointingIn, ArrowsPointingOut } from "@steeze-ui/heroicons";
+	import { Icon } from "@steeze-ui/svelte-icon";
 
     export let href = '';
     export let title: string | undefined = undefined;
     export let text = '';
+
+    let expanded = false;
+
 </script>
 
-<img 
-    src={`help/${$lang}/${href}`} 
-    {title} 
-    alt={text} 
-    class="my-1 border-[1px] border-indigo-500/50 rounded-md max-w-[66%] mx-auto"
->
+<Flex direction="row" items="center" justify="center">
+    <div style:max-width={expanded ? '100%' : '66%'} class="relative duration-300">
+        <img 
+            src={`help/${$lang}/${href}`} 
+            {title} 
+            alt={text} 
+            class="my-1 border-[1px] border-indigo-500/50 rounded-md mx-auto duration-300"
+        >
+        <button on:click={() => expanded = !expanded} class="absolute bottom-6 right-6" name="Expand image">
+            <Icon src={!expanded ? ArrowsPointingOut : ArrowsPointingIn} class="h-8 w-8 text-zinc-800 bg-white rounded-md p-1 border border-zinc-500" />
+        </button>
+    </div>
+</Flex>

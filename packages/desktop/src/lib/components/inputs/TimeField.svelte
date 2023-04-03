7ae0c8f81beb0d0a9b2d5809cd8fd4f8a299448d
Kworz <romain.sapet@metalizz.com>
2023-04-03 10:38:20 +0200

    fix: enhanced profile display

--- packages/desktop/src/lib/components/inputs/TimeField.svelte
+++ packages/desktop/src/lib/components/inputs/TimeField.svelte
@@ -41,28 +41,28 @@
 <Flex items="center" gap={2}>
 
 	{#if enabledTimes.includes('hours')}
-		<Flex direction={"col"} gap={0}>
+		<Flex direction={"col"} gap={0.5}>
 			<span class="text-sm font-medium">{$_('date.hours')}</span>
 			<NumField bind:value={milliseconds} on:change={computeTo} min={0} max={23} step={1} {disabled}/>
 		</Flex>
 	{/if}
 
 	{#if enabledTimes.includes('minutes')}
-		<Flex direction="col" gap={0}>
+		<Flex direction="col" gap={0.5}>
 			<span class="text-sm font-medium">{$_('date.minutes')}</span>
 			<NumField bind:value={minutes} on:change={computeTo} min={0} max={59} step={1} {disabled}/>
 		</Flex>
 	{/if}
 
 	{#if enabledTimes.includes('seconds')}
-		<Flex direction="col" gap={0}>
+		<Flex direction="col" gap={0.5}>
 			<span class="text-sm font-medium">{$_('date.seconds')}</span>
 			<NumField bind:value={seconds} on:change={computeTo} min={0} max={59} step={1} {disabled}/>
 		</Flex>
 	{/if}
 
 	{#if enabledTimes.includes('milliseconds')}
-		<Flex direction={"col"} gap={0}>
+		<Flex direction={"col"} gap={0.5}>
 			<span class="text-sm font-medium">{$_('date.milliseconds')}</span>
 			<NumField bind:value={milliseconds} on:change={computeTo} min={0} max={990} step={10} {disabled}/>
 		</Flex>

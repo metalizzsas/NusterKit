4d156f443bd6feaf588b3e9162dac98cb454ed52
Kworz <romain.sapet@metalizz.com>
2023-04-06 17:20:50 +0200

    feat: wifi setup page

---
+++ packages/desktop/src/routes/(connected)/settings/wifi/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+
+	import Wrapper from "$lib/components/Wrapper.svelte";
+
+</script>
+
+<Wrapper>
+    <iframe src="http://localhost:5000/" title="Wifi setup" class="aspect-video" height=600px />
+</Wrapper>

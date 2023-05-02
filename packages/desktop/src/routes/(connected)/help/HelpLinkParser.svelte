272af2d943b30489162b1452a36a754d7754cb6f
Kworz <romain.sapet@metalizz.com>
2023-05-02 12:09:47 +0200

    fix: help relative path

--- packages/desktop/src/routes/(connected)/help/HelpLinkParser.svelte
+++ packages/desktop/src/routes/(connected)/help/HelpLinkParser.svelte
@@ -4,7 +4,21 @@
 
     export let href = '';
     const selectedHelp = getContext<Writable<string | undefined>>("help");
+
+    const navigate = (where: string) => {
+
+        let pathToGo = ($selectedHelp ?? "").split("/");
+        const folderAway = [...href.matchAll(new RegExp(/\.\.\//, "g"))].length;
+
+        const relativePath = where.split("/").filter(k => k !== "..");
+        const basePath = pathToGo?.slice(1, pathToGo.length - (1 + folderAway))
+
+        const newPathToGo = [...basePath, ...relativePath];
+
+        $selectedHelp = "/" + newPathToGo.join("/");        
+    }
+
 </script>
 
-<button on:click={() => $selectedHelp = href } class="text-indigo-500 font-medium hover:text-indigo-600 duration-100"><slot /></button>
+<button on:click={() => navigate(href)} class="text-indigo-500 font-medium hover:text-indigo-600 duration-100"><slot /></button>
   

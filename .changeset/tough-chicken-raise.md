942fe3a38ba2113ac7afaa61c8b4ebf42cb874ad
Kworz <romain.sapet@metalizz.com>
2023-06-14 09:18:40 +0200

    feat: sensor maintenance units

---
+++ .changeset/tough-chicken-raise.md
@@ -0,0 +1,6 @@
+---
+"@metalizzsas/nuster-desktop": patch
+"@metalizzsas/nuster-turbine": patch
+---
+
+feat: sensor maintenance now shows units
--- libs/typings/src/hydrated/maintenance/index.ts
+++ libs/typings/src/hydrated/maintenance/index.ts
@@ -1,13 +1,21 @@
-import type { BaseMaintenance } from "../../spec/maintenances";
+type BaseMaintenanceHydrated = {
 
-export type MaintenanceHydrated = {
-
-    name: string;
-
-    durationType: BaseMaintenance["durationType"]
+    name: string; //Maintenance task name
 
     duration: number; // Actual value
     durationMax: number; // Maximum value
 
     durationProgress: number; // Float value 0-1
 }
+
+type CountableMaintenance = {
+    durationType: "cycle" | "duration"
+}
+
+type SensorMaintenance = {
+    durationType: "sensor";
+
+    sensorUnit?: string;
+}
+
+export type MaintenanceHydrated = BaseMaintenanceHydrated & (CountableMaintenance | SensorMaintenance)

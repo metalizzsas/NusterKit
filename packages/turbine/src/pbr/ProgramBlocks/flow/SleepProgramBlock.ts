e0a5c4c686e04b6e44061d89fe80dbe397d47529
Kworz <romain.sapet@metalizz.com>
2023-08-18 14:51:24 +0200

    sleep programblock is the only programblock that can be paused

--- packages/turbine/src/pbr/ProgramBlocks/flow/SleepProgramBlock.ts
+++ packages/turbine/src/pbr/ProgramBlocks/flow/SleepProgramBlock.ts
@@ -8,6 +8,11 @@ export class SleepProgramBlock extends ProgramBlock
 {    
     sleepTime: NumericParameterBlockHydrated;
 
+    iterationsSkippedByPause = 0;
+    currentSleepTime = 0;
+
+    sleepStarted = false;
+
     constructor(obj: SleepProgramBlockSpec)
     {
         super(obj);
@@ -15,29 +20,44 @@ export class SleepProgramBlock extends ProgramBlock
         this.sleepTime = ParameterBlockRegistry.Numeric(obj.sleep);
         this.estimatedRunTime = this.sleepTime.data;
 
-        TurbineEventLoop.on(`pbr.stop`, () => this.earlyExit = true);
         TurbineEventLoop.on(`pbr.status.update`, (state) => { if (state === "ended" || state === "ending") { this.earlyExit = true }});
+
+        TurbineEventLoop.on('pbr.resume', () => {
+            if(this.sleepStarted && !this.executed)
+            {
+                TurbineEventLoop.emit("log", "info", `SleepBlock: Resuming, paused during ${this.iterationsSkippedByPause * 10} ms. Slept ${this.currentSleepTime * 10} / ${this.sleepTime.data * 1000 + this.iterationsSkippedByPause * 10} ms.`);
+            }
+        });
     }
 
     async execute(signal?: AbortSignal): Promise<void>
-    {        
+    {
+        this.sleepStarted = true;
+        this.iterationsSkippedByPause = 0;
+
         const timeStart = performance.now();
         const sleepTime = this.sleepTime.data * 1000;
         
         TurbineEventLoop.emit("log", "info", `SleepBlock: Will sleep for ${sleepTime} ms.`);
+        TurbineEventLoop.emit("pbr.setPausable", true);
 
-        for (let i = 0; i < ((sleepTime) / 10); i++)
+        for (this.currentSleepTime = 0; this.currentSleepTime < ((sleepTime) / 10) + this.iterationsSkippedByPause; this.currentSleepTime++)
         {
             const timeLoop = performance.now();
 
+            if(this.paused)
+                this.iterationsSkippedByPause++;
+
             if (this.earlyExit === true || signal?.aborted === true)
                 break;
-            if(timeLoop - timeStart >= sleepTime)
+            
+            if((timeLoop - timeStart) >= (sleepTime + (this.iterationsSkippedByPause * 10)))
                 break;
-            else
-                await new Promise(resolve => { setTimeout(resolve, 10); });
+
+            await new Promise(resolve => { setTimeout(resolve, 10); });
         }
 
+        TurbineEventLoop.emit("pbr.setPausable", false);
         this.executed = true;
     }
 

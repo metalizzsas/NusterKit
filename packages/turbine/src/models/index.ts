9e114ea1f0412422f8c634b90eb2f65f4895b1cb
Kworz <romain.sapet@metalizz.com>
2023-09-06 11:00:37 +0200

    removed models

--- packages/turbine/src/models/index.ts
+++
@@ -1,5 +0,0 @@
-import { MaintenanceModel } from "./MaintenanceModel";
-import { ProfileModel } from "./ProfileModel";
-import { ContainerModel } from "./ContainerModel";
-
-export { MaintenanceModel, ProfileModel, ContainerModel };

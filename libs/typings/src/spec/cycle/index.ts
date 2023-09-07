44a82314b777fbd3bd189aea9c7865e9314bf30d
Kworz <romain.sapet@metalizz.com>
2023-09-07 10:29:22 +0200

    Added comment on premade name

--- libs/typings/src/spec/cycle/index.ts
+++ libs/typings/src/spec/cycle/index.ts
@@ -1,8 +1,11 @@
 import type { ProgramBlockRunner } from "./ProgramBlockRunner";
 
 type CyclePremade = {
+    /** Name is only for description purposes, not used */
     name: string;
+    /** Cycle type name */
     cycle: string;
+    /** Profile UUID used by this premade */
     profile?: string;
 };
 

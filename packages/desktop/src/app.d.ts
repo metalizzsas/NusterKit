e454d161092880a29351e52cd98e236019bd73c5
Kworz <romain.sapet@metalizz.com>
2024-05-24 11:24:52 +0200

    global types fix

--- packages/desktop/src/app.d.ts
+++ packages/desktop/src/app.d.ts
@@ -1,35 +1,39 @@
+/// <reference types="@sveltejs/kit" />
+
 import type { Status } from "@metalizzsas/nuster-turbine/types";
 import type { MachineData } from "@metalizzsas/nuster-turbine/types/hydrated/machine";
 
-declare namespace App
-{
-    interface Locals {
-
-        /** Machine settings stored in turbine */
-        settings: { dark: 1 | 0, lang: string }
-
-        /** Is machine screen, in dev mode, will always be true */
-        is_machine_screen: boolean;
-        
-        /** Actual machine configuration, read from TURBINE_ADDRESS API */
-        machine_configuration: MachineData;
-
-        /** Actual machine realtime data on page load, read from TURBINE_ADDRESS Realtime API */
-        machine_status: Status; 
-    }
+declare global {
+    namespace App
+    {
+        interface Locals {
     
-    interface PageData {
-
-        /** Machine settings stored in turbine */
-        settings: { dark: 1 | 0, lang: string }
-
-        /** Is machine screen, in dev mode, will always be true */
-        is_machine_screen: boolean;
-
-        /** Actual machine configuration, read from TURBINE_ADDRESS API */
-        machine_configuration: MachineData;
-
-        /** Actual machine realtime data on page load, read from TURBINE_ADDRESS Realtime API */
-        machine_status: Status;
+            /** Machine settings stored in turbine */
+            settings: { dark: 1 | 0, lang: string }
+    
+            /** Is machine screen, in dev mode, will always be true */
+            is_machine_screen: boolean;
+            
+            /** Actual machine configuration, read from TURBINE_ADDRESS API */
+            machine_configuration: MachineData;
+    
+            /** Actual machine realtime data on page load, read from TURBINE_ADDRESS Realtime API */
+            machine_status: Status; 
+        }
+        
+        interface PageData {
+    
+            /** Machine settings stored in turbine */
+            settings: { dark: 1 | 0, lang: string }
+    
+            /** Is machine screen, in dev mode, will always be true */
+            is_machine_screen: boolean;
+    
+            /** Actual machine configuration, read from TURBINE_ADDRESS API */
+            machine_configuration: MachineData;
+    
+            /** Actual machine realtime data on page load, read from TURBINE_ADDRESS Realtime API */
+            machine_status: Status;
+        }
     }
 }
--- packages/desktop/vite.config.ts
+++ packages/desktop/vite.config.ts
@@ -2,22 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 export default {
-    plugins: [
-        sveltekit()
-    ],
+    plugins: [sveltekit()],
     server: {
         port: 4081,
         host: "0.0.0.0",
         hmr: {
             path: "/__vite_hmr",
             port: 9026
-        }
-    },
-    build: {
-        rollupOptions: {
-            external: [
-                "/version.txt?raw"
-            ]
         }
     }
 } satisfies UserConfig;

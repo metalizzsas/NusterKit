8d1f5ef69686b0d5aa050939ba9ed78b2d2e3d27
Kworz <romain.sapet@metalizz.com>
2024-07-01 10:20:38 +0200

    updated progress bar

--- packages/ui/src/lib/components/ProgressBar.svelte
+++ packages/ui/src/lib/components/ProgressBar.svelte
@@ -3,45 +3,38 @@
     import { tweened } from "svelte/motion";
     import { cubicOut} from 'svelte/easing';
 
-    export let progress: number;
+    export let progress: number | null;
 
-    export let showNumbers = false;
-
-    export let dots: number | undefined = undefined;
+    export let showProgressLabel = false;
     
     let springProgress = tweened(0, {
 		duration: 500,
 		easing: cubicOut
 	});
 
-    $: void springProgress.set(progress);
+    $: void springProgress.set(progress ?? 1);
 
 </script>
 
-<div class="bg-zinc-600/50 h-1.5 rounded-full grow relative" class:mt-4={showNumbers}>
-
-    {#if showNumbers && progress !== -1}
-        <span class="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 bg-indigo-500 rounded-full py-0.5 px-3 text-xs text-white font-medium">{Math.floor(progress * 100)} %</span>
+<div class="bg-zinc-600/50 h-1.5 rounded-full grow relative" class:my-2={showProgressLabel}>
+    {#if showProgressLabel}
+        <span 
+            class:bg-indigo-500={progress !== null}
+            class:bg-violet-500={progress === null}
+            class="absolute z-10 left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 rounded-full py-0.5 px-3 text-xs text-white font-medium"
+        >
+            {#if progress === null}
+                — %
+            {:else}
+                {Math.floor((progress ?? 1) * 100)} %
+            {/if}
+        </span>
     {/if}
-
-    {#if dots}
-        {#each Array.from(Array(dots).keys()) as dot}
-            <div 
-                class="h-1.5 w-1.5 absolute top-0 bg-white/50 rounded-full z-10" 
-                style:left="{(1 / dots) * 100 + (dot / dots) * 100}%"
-                class:invisible={(dot + 2 > dots)}
-                style:transform={"translateX(-50%)"}
-            />
-        {/each}
-    {/if}
-    {#if progress !== -1}
-        <div 
-            class="h-1.5 rounded-full z-20 bg-indigo-500"
-            style:width={`${$springProgress * 100}%`}
-        />
-    {:else}
-        <div
-            class="h-1.5 rounded-full z-20 bg-violet-500 animate-pulse w-full"
-        />
-    {/if}
+    <div 
+        class="h-1.5 rounded-full z-20"
+        class:bg-indigo-500={progress !== null}
+        class:bg-violet-500={progress === null}
+        class:animate-pulse={progress === null}
+        style:width={`${$springProgress * 100}%`}
+    />
 </div>
---
+++ packages/ui/src/lib/components/ProgressBarGroup.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+
+	import ProgressBar from "./ProgressBar.svelte";
+
+    export let progresses: Array<number | null>;
+    export let showProgressLabelForIndex;
+
+</script>
+
+<div class="relative">
+    <div class="flex gap-4 items-center">
+        {#each progresses as progress, index}
+            <ProgressBar progress={progress} showProgressLabel={showProgressLabelForIndex === index} />
+        {/each}
+    </div>
+</div>
